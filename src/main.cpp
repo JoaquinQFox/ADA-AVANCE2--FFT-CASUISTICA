1c09871a8181b6e765f6ff088c72816de454303d
leonhatches <jhatches@unsa.edu.pe>
2025-11-16 19:55:51 -0500

    Se crea la función para leer el audio que solo obtiene la muestra de audio

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
+#define DR_WAV_IMPLEMENTATION
+#include "dr_wav.h"
+#include <cstdint>
 #include <iostream>
-
+#include <vector>
+#include <cmath>
 using namespace std;
 
 // Lectura de audio
+vector <double> cargar_normalizar_wav (const char* filename) {
+    
+    drwav wav;
 
+    // Lectura de muestras de audios WAV
+    vector<int16_t> samples(wav.totalPCMFrameCount);
+    drwav_read_pcm_frames_s16(&wav, wav.totalPCMFrameCount, samples.data());
+    drwav_uninit(&wav);
+}
 
 
 // FFT
