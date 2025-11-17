e8c16c46314a6e44800299b6e103b2f79ef1c621
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-11-16 19:14:40 -0500

    Se crea main con estructura para implementar algoritmos

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+// Lectura de audio
+
+
+
+// FFT
+
+
+
+// Filtrado de frecuencias cardiacas
+
+
+
+// IFFT
+
+
+
+// Extracción de BPM
+
+
+
+// Detección de anomalias
+
+
+// Main
